73e8b6a62f0105e48dd322161e4bbf790ab330f9
omartins-zs <gabrielmatheus209@gmail.com>
2023-10-29 23:32:11 -0300

    Add Script da Tabela de Perfumes

---
+++ Scripts/Perfumes.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE fragance_euphoria_bd;
+
+CREATE TABLE `fragance_euphoria_bd`.`perfumes` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`descricao` VARCHAR(100) NOT NULL,
+	`marca` INT NOT NULL,
+	`tipo` ENUM ('Masculino', 'Feminino', 'Unissex'),
+	`volume` INT NOT NULL,
+	`preco` DECIMAL(10, 2) NOT NULL,
+	`estoque` INT NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
