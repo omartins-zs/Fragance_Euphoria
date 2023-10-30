3bfe24079e2ecb3743c9be7f73f1d9bdefcb6606
omartins-zs <gabrielmatheus209@gmail.com>
2023-10-30 16:38:28 -0300

    Criando Card que será usado para Clientes

--- application/views/admin/admin.php
+++ application/views/admin/admin.php
@@ -91,6 +91,25 @@
 				</div>
 			</div>
 
+
+			<!-- Pending Requests Card Example -->
+			<div class="col-xl-3 col-md-6 mb-4">
+				<div class="card border-left-dark shadow h-100 py-2">
+					<div class="card-body">
+						<div class="row no-gutters align-items-center">
+							<div class="col mr-2">
+								<div class="text-xs font-weight-bold text-dark text-uppercase mb-1">
+									Clientes</div>
+								<div class="h5 mb-0 font-weight-bold text-gray-800">30</div>
+							</div>
+							<div class="col-auto">
+								<i class="fas fa-users fa-2x text-gray-300"></i>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
 		</div>
 
 	</div>
