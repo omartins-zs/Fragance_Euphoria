37960827261ac0e3b560adc1d1046dd33c5661c8
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-15 01:28:21 -0300

    Criando Controller de Cliente

---
+++ application/controllers/admin/Cliente.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Cliente extends MY_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->model('Categoria_model');
+		// $this->load->library('form_validation');
+		// $this->load->library('session');
+		// $this->session->userdata('loggedin') == TRUE || redirect('user/login');
+	}
+
+	public function index()
+	{
+		$dados['clientes'] = $this->Cliente_model->buscaclientes();
+
+		$dados['subview'] = 'admin/cliente/listagem';
+
+		$this->load->vars($dados);
+
+		$this->load->view('admin/layout_main_admin');
+	}
+}
