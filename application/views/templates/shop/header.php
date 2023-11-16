cef5b5275755a884f728dd4dfbebccdae3381f51
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-16 17:25:19 -0300

    Criando Template Header do Shop

---
+++ application/views/templates/shop/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bootstrap demo</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+	<!-- Font Awesome - Icons -->
+	<script src="https://kit.fontawesome.com/6739ae1e1b.js" crossorigin="anonymous"></script>
+
+	<link href="<?= base_url() ?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
