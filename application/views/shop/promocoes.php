dbef5eefa60e5d8e0d066183dc0a75ff9168aaa5
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-16 17:26:30 -0300

    Criando View de Promoções e Cards com IMGs

---
+++ application/views/shop/promocoes.php
@@ -0,0 +1,22 @@
+<div class="container">
+	<div class="d-flex justify-content-around mt-4">
+		<div class="card text-center" style="width: 20rem;">
+			<img src="<?= base_url() ?>assets/shop/img/compre_ganhe_brinde.png" class="card-img-top" alt="...">
+			<div class="card-body">
+				<p class="card-text">Compre e Ganhe um Brinde</p>
+			</div>
+		</div>
+		<div class="card text-center" style="width: 20rem;">
+			<img src="<?= base_url() ?>assets/shop/img/compre2_leve3.png" class="card-img-top" alt="...">
+			<div class="card-body">
+				<p class="card-text">Compre 2, Leve 3</p>
+			</div>
+		</div>
+		<div class="card text-center" style="width: 20rem;">
+			<img src="<?= base_url() ?>assets/shop/img/black_friday.png" class="card-img-top" alt="...">
+			<div class="card-body">
+				<p class="card-text">Black Friday</p>
+			</div>
+		</div>
+	</div>
+</div>
