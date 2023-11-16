f0ecace08ce9f5ed166247c0908195a01d0fc6fe
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-16 17:25:45 -0300

    Criando Template Footer do Shop

---
+++ application/views/templates/shop/footer.php
@@ -0,0 +1,81 @@
+<footer class="border-top ">
+		<div class="container mt-4">
+			<div class="row">
+				<div class="col-lg-6 mb-5 mb-lg-0">
+					<div class="row">
+						<div class="col-md-12">
+							<h3 class="footer-heading mb-4">Navigations</h3>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<ul class="list-unstyled">
+								<li><a href="#">Sell online</a></li>
+								<li><a href="#">Features</a></li>
+								<li><a href="#">Shopping cart</a></li>
+								<li><a href="#">Store builder</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<ul class="list-unstyled">
+								<li><a href="#">Mobile commerce</a></li>
+								<li><a href="#">Dropshipping</a></li>
+								<li><a href="#">Website development</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<ul class="list-unstyled">
+								<li><a href="#">Point of sale</a></li>
+								<li><a href="#">Hardware</a></li>
+								<li><a href="#">Software</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+					<h3 class="footer-heading mb-4">Promoção</h3>
+					<a href="#" class="block-6">
+						<img src="<?= base_url() ?>assets/img/promocao_valor_perfume.png" alt="Image placeholder" class="img-fluid mb-2">
+						<h6 class="font-weight-light mb-0">Encontre seu Perfume preferido</h6>
+						<p>Promoção de 15 de Janeiro - 25 <?= date('Y') ?></p>
+					</a>
+				</div>
+				<div class="col-md-6 col-lg-3">
+					<div class="block-5 mb-5">
+						<h4 class="footer-heading mb-4">Informações de Contato</h4>
+						<ul class="list-unstyled">
+							<li><i class="fas fa-map-marker-alt mr-1"></i>Rua do teste N°23, Bairro do Teste, Teste, BR</li>
+							<li><i class="fas fa-phone mr-1"></i> <a href="tel://23923929210">11 99999-8888</a></li>
+							<li><i class="fas fa-envelope mr-1"></i>emailaddress@domain.com</li>
+						</ul>
+					</div>
+					<div class="block-7">
+						<form action="#" method="post">
+							<label for="email_subscribe" class="footer-heading">Subscribe</label>
+							<div class="input-group mb-3">
+								<input type="text" class="form-control" placeholder="Email" aria-label="Recipient's username" aria-describedby="button-addon2">
+								<button class="btn btn-primary" type="button" id="button-addon2">Send</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="row pt-5 mt-5 text-center">
+				<div class="col-md-12">
+					<p>
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						Copyright &copy;
+						<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+						<?= date('Y') ?> All rights reserved | This
+						template is made
+						with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					</p>
+				</div>
+
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
