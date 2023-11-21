44e03f988d091ea8b3cfe52e939222d9a526a4cf
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-21 00:07:53 -0300

    Ajuste dos Cards de Produtos com Informações

--- application/views/shop/produtos.php
+++ application/views/shop/produtos.php
@@ -1,238 +1,31 @@
 <section class="py-5">
 	<div class="container px-4 px-lg-5 mt-5">
 		<div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
+			<?php foreach ($perfumes as $perfume) : ?>
+				<div class="col mb-5">
+					<div class="card h-100">
+						<!-- Product image-->
+						<img class="card-img-top" style="width: 243px; height:162px;object-fit: contain;" src="<?= !empty($perfume->imagem) ? base_url('assets/admin/upload/' . $perfume->imagem) : base_url('assets/img/perfume_default.png'); ?>" alt="<?= $perfume->descricao; ?>" />
+						<!-- Product details-->
+						<div class="card-body p-2">
+							<div class="text-center">
+								<!-- Product name-->
+								<h4 class="fw-bolder my-0"><?= $perfume->descricao; ?></h4>
+								<h6 class="fw-light mb-4"><?= nome_marca($perfume->marca); ?></h6>
+								<h5 class="fw-bolder mb-1 text-info">R$ <?= $perfume->preco ?></h5>
+								<p class="lead my-0"><?= $perfume->volume; ?> ML</p>
+							</div>
 						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
+						<!-- Product actions-->
+						<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
+							<div class="btn-group">
+								<button type="button" class="btn btn-outline-secondary">View</button>
+								<button type="button" class="btn btn-outline-secondary">Edit</button>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col mb-5">
-				<div class="card h-100">
-					<!-- Product image-->
-					<img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-					<!-- Product details-->
-					<div class="card-body p-4">
-						<div class="text-center">
-							<!-- Product name-->
-							<h5 class="fw-bolder mb-4">Special Item</h5>
-							<!-- Product reviews-->
-							<!-- <div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-								<div class="fas fa-star" style="color: #ffc107;"></div>
-							</div> -->
-							$40.00
-						</div>
-					</div>
-					<!-- Product actions-->
-					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent mx-auto">
-						<div class="btn-group">
-							<button type="button" class="btn btn-outline-secondary">View</button>
-							<button type="button" class="btn btn-outline-secondary">Edit</button>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </section>
