2a52121e3982654ba6c9f9d598d0cebde520068b
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-22 11:12:46 -0300

    Add Form para Adicionar o Produto no Carrinho

--- application/views/shop/detalhes.php
+++ application/views/shop/detalhes.php
@@ -13,11 +13,16 @@
 				</div>
 				<p class="lead text-justify"><?= $perfume->descricao_info; ?></p>
 				<div class="d-flex">
-					<input class="form-control text-center me-3" id="inputQuantity" type="" value="1" style="max-width: 3rem" />
-					<button class="btn btn-outline-dark flex-shrink-0" type="button">
-						<i class="fas fa-shopping-cart"></i>
-						Adicionar no carrinho
-					</button>
+					<!-- Formulário para adicionar ao carrinho -->
+					<form action="<?= base_url('Carrinho/adicionar'); ?>" method="post">
+						<input type="hidden" name="id" value="<?= $perfume->id; ?>">
+						<input type="hidden" name="nome" value="<?= $perfume->nome; ?>">
+						<input type="hidden" name="preco" value="<?= $perfume->preco; ?>">
+						<label for="quantidade">Quantidade:</label>
+						<input class="form-control text-center me-3" id="quantidade" name="quantidade" type="text" value="1" style="max-width: 3rem" />
+						<input type="submit" class="btn btn-outline-dark flex-shrink-0" value="Adicionar no carrinho">
+					</form>
+					<!-- Fim do formulário -->
 				</div>
 			</div>
 		</div>
