deedf10cf12673e313ba51be39d4fb024f0082b2
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-22 11:11:14 -0300

    Criando Function para Adicionar Item no Carrinho

--- application/controllers/Carrinho.php
+++ application/controllers/Carrinho.php
@@ -10,4 +10,26 @@ class Carrinho extends MY_Controller
 		parent::__construct();
 		$this->load->library('cart');
 	}
+	public function index()
+	{
+		$this->load->view('templates/shop/header');
+		$this->load->view('templates/shop/navbar');
+		// Exibe a página do carrinho
+		$this->load->view('shop/carrinho');
+		$this->load->view('templates/shop/footer');
+	}
+
+	public function adicionar()
+	{
+		// Adiciona um item ao carrinho
+		$data = array(
+			'id'      => $this->input->post('id'),
+			'qtde'     => $this->input->post('quantidade'),
+			'preco'   => $this->input->post('preco'),
+			'nome'    => $this->input->post('nome'),
+		);
+
+		$this->cart->insert($data);
+		redirect('carrinho');
+	}
 }
