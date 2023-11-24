b50e758fd7498b09b7888ace8b0e161cdc69b6ff
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-23 23:19:21 -0300

    Ajustando Header e Add Comentarios

--- application/views/templates/shop/header.php
+++ application/views/templates/shop/header.php
@@ -5,13 +5,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Bootstrap demo</title>
+
+	<!-- Bootstrap -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 	<!-- Font Awesome - Icons -->
 	<script src="https://kit.fontawesome.com/6739ae1e1b.js" crossorigin="anonymous"></script>
 
-	<link href="<?= base_url() ?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
-
+	<!-- CSS Personalizado -->
 	<link rel="stylesheet" href="<?= base_url() ?>assets/shop/css/style.css">
 
 </head>
