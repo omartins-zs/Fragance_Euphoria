43d7bc54d622ead33ced3b2a0a0296aa03be7510
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-26 22:11:19 -0300

    For Each dos Perfumes no Carrinho e Ajuste Layout

--- application/views/shop/carrinho.php
+++ application/views/shop/carrinho.php
@@ -13,6 +13,25 @@
 							<div class="col text-right text-muted"><?= $produtosNoCarrinho ?> items</div>
 						</div>
 					</div>
+					<div class="row border-top border-bottom">
+						<?php foreach ($perfumesDoCarrinho as $index => $perfume) : ?>
+							<div class="row main align-items-center my-1">
+								<div class="col-2"><img class="img-fluid" src="<?= !empty($perfume['imagem']) ? base_url('assets/admin/upload/' . $perfume['imagem']) : base_url('assets/img/perfume_default.png'); ?>"></div>
+								<div class="col">
+									<div class="row text-muted"><?= $perfume['nome']; ?></div>
+								</div>
+								<div class="col">
+									<span class="text-decoration-none" href="#"><?= $perfume['qtde']; ?></span>
+								</div>
+								<!-- <div class="col">R$ 44.00 <span class="close">&#10005;</span></div> -->
+								<div class="col text-right">R$ <?= $perfume['preco']; ?> <a href="<?= base_url('carrinho/remover/' . $index); ?>"><span class="close">&#10005;</span></a></div>
+							</div>
+						<?php endforeach; ?>
+					</div>
+					<div class="d-flex justify-content-between my-3">
+						<div class="back-to-shop"><a class="text-decoration-none" href="<?= base_url('shop'); ?>"><i class="fas fa-arrow-left"></i><span class="text-muted">Voltar para as compras</span></a></div>
+						<a href="<?= base_url('carrinho/limpar'); ?>" class="btn btn-warning btn-sm btn-block">Limpar Carrinho</a>
+					</div>
 				</div>
 			</div>
 		</div>
