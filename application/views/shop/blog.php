f6c806ed9598b42b725d9053f262a22ba2cdf885
omartins-zs <gabrielmatheus209@gmail.com>
2023-11-28 23:42:24 -0300

    Adicionado o Blog com Embed do Youtube

---
+++ application/views/shop/blog.php
@@ -0,0 +1,143 @@
+ <main role="main">
+
+ 	<!-- Main jumbotron for a primary marketing message or call to action -->
+ 	<div class="jumbotron">
+ 		<div class="container">
+ 			<h1 class="display-3">Cuidados com a pele ToNaFragancee</h1>
+ 			<p>Utilizar somente um ótimo perfume grande parte das vezes nem sempre é o suficiente, ou seja, é necessário
+ 				alguns cuidados com a aparência para te deixar mais jovial ou mais atraente</p>
+ 			<p><a class="btn btn-primary btn-lg" href="https://youtu.be/PLUorDoO1t0?si=-HxSspuPSsBUCCR9" role="button">Dicas de skin care! &raquo;</a></p>
+ 		</div>
+ 	</div>
+
+ 	<div class="container">
+ 		<!-- Example row of columns -->
+ 		<div class="row">
+
+ 			<div class="col-md-6">
+ 				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative videoDicaBlog">
+ 					<div class="col-6 p-4 d-flex flex-column position-static">
+ 						<!-- <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong> -->
+ 						<h3 class="mb-0">Limpeza</h3>
+ 						<div class="mb-1 text-body-secondary">Nov 12</div>
+ 						<p class="card-text mb-auto text-justify">A hidratação é o passo mais importante de qualquer rotina de cuidados com a pele. Ela ajuda a manter a
+ 							pele hidratada e saudável.</p>
+ 						<a href="https://www.youtube.com/watch?v=uPWMIR2vpLM&pp=ygUccG9ycXVlIHNlbXByZSBsaW1wYXIgYSBwZWxlIA%3D%3D" class="icon-link gap-1 icon-link-hover stretched-link">
+ 							Ver video da Dica
+ 							<i class="fas fa-chevron-right"></i>
+
+ 						</a>
+ 					</div>
+ 					<div class="col-auto d-none d-lg-block">
+ 						<iframe height="100%" src="https://www.youtube.com/embed/uPWMIR2vpLM?si=EA7vSjNxFgARs47P" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ 					</div>
+ 				</div>
+ 			</div>
+
+ 			<div class="col-md-6">
+ 				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative videoDicaBlog">
+ 					<div class="col-6 p-4 d-flex flex-column position-static">
+ 						<!-- <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong> -->
+ 						<h3 class="mb-0">Esfoliação</h3>
+ 						<div class="mb-1 text-body-secondary">Nov 12</div>
+ 						<p class="card-text mb-auto text-justify">A esfoliação ajuda a remover as células mortas da pele, o que pode ajudar a melhorar a textura da pele e
+ 							a reduzir a aparência de manchas e cicatrizes.</p>
+ 						<a href="https://www.youtube.com/watch?v=sxkCW7Xpc1Q&pp=ygUXcG9ycXVlIGVzZm9saWFyIGEgcGVsZSA%3D" class="icon-link gap-1 icon-link-hover stretched-link">
+ 							Ver video da Dica
+ 							<i class="fas fa-chevron-right"></i>
+
+ 						</a>
+ 					</div>
+ 					<div class="col-auto d-none d-lg-block">
+ 						<iframe height="100%" src="https://www.youtube.com/embed/sxkCW7Xpc1Q?si=56FEBIJdqzTfrwrH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<hr>
+
+ 		<div class="row">
+ 			<div class="col-md-6">
+ 				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative videoDicaBlog">
+ 					<div class="col-6 p-4 d-flex flex-column position-static">
+ 						<!-- <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong> -->
+ 						<h3 class="mb-0">Máscaras faciais</h3>
+ 						<div class="mb-1 text-body-secondary">Nov 12</div>
+ 						<p class="card-text mb-auto text-justify">As máscaras faciais podem ajudar a tratar problemas específicos da pele, como acne, manchas ou rugas.</p>
+ 						<a href="https://www.youtube.com/watch?v=LEBEew_GEuc&pp=ygUecG9ycXVlIHVzYXIgbcOhc2NhcmFzIGZhY2lhaXMg" class="icon-link gap-1 icon-link-hover stretched-link">
+ 							Ver video da Dica
+ 							<i class="fas fa-chevron-right"></i>
+
+ 						</a>
+ 					</div>
+ 					<div class="col-auto d-none d-lg-block">
+ 						<iframe height="100%" src="https://www.youtube.com/embed/LEBEew_GEuc?si=PqGef6zCB1TIg29M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<div class="col-md-6">
+ 				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative videoDicaBlog">
+ 					<div class="col-6 p-4 d-flex flex-column position-static">
+ 						<!-- <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong> -->
+ 						<h3 class="mb-0">Beba bastante água</h3>
+ 						<div class="mb-1 text-body-secondary">Nov 12</div>
+ 						<p class="card-text mb-auto text-justify">Uma alimentação saudável pode ajudar a manter a pele saudável de dentro para fora. Inclua frutas,
+ 							vegetais, cereais integrais e proteínas magras em sua dieta.</p>
+ 						<a href="https://www.youtube.com/watch?v=q7R2mS0DONQ&pp=ygUjYmViZXIgYmFzdGFudGUgYWd1YSBtZWxob3JhIGEgcGVsZSA%3D" class="icon-link gap-1 icon-link-hover stretched-link">
+ 							Ver video da Dica
+ 							<i class="fas fa-chevron-right"></i>
+
+ 						</a>
+ 					</div>
+ 					<div class="col-auto d-none d-lg-block">
+ 						<iframe height="100%" height="315" src="https://www.youtube.com/embed/q7R2mS0DONQ?si=o6S4UdrKP7RcWwj6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 		<hr>
+
+ 		<div class="row">
+ 			<div class="col-md-6">
+ 				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative videoDicaBlog">
+ 					<div class="col-6 p-4 d-flex flex-column position-static">
+ 						<!-- <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong> -->
+ 						<h3 class="mb-0">Proteção solar</h3>
+ 						<div class="mb-1 text-body-secondary">Nov 12</div>
+ 						<p class="card-text mb-auto text-justify">A proteção solar é o passo mais importante de qualquer rotina de cuidados com a pele. Ela ajuda a
+ 							proteger a pele dos danos causados pelos raios solares, como rugas, manchas e câncer de pele.</p>
+ 						<a href="https://www.youtube.com/watch?v=9BivKdTz16c&pp=ygUZcG9ycXVlIGRvIHByb3RldG9yIHNvbGFyIA%3D%3D" class="icon-link gap-1 icon-link-hover stretched-link">
+ 							Ver video da Dica
+ 							<i class="fas fa-chevron-right"></i>
+
+ 						</a>
+ 					</div>
+ 					<div class="col-auto d-none d-lg-block">
+ 						<iframe height="100%" src="https://www.youtube.com/embed/9BivKdTz16c?si=0mbkpBLPraYlB3Db" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<div class="col-md-6">
+ 				<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative videoDicaBlog">
+ 					<div class="col-6 p-4 d-flex flex-column position-static">
+ 						<!-- <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong> -->
+ 						<h3 class="mb-0">Hidratação</h3>
+ 						<div class="mb-1 text-body-secondary">Nov 12</div>
+ 						<p class="card-text mb-auto text-justify">A limpeza é o passo mais importante de qualquer rotina de cuidados com a pele. Ela remove a sujeira, a oleosidade e as impurezas que podem obstruir os poros e causar acne, manchas e outros problemas de pele.</p>
+ 						<a href="https://www.youtube.com/watch?v=8eekJ-SarQo&pp=ygUdZ3VpYSBkZSBoaWRyYXRhw6fDo28gZGUgcGVsZSA%3D" class="icon-link gap-1 icon-link-hover stretched-link">
+ 							Ver video da Dica
+ 							<i class="fas fa-chevron-right"></i>
+
+ 						</a>
+ 					</div>
+ 					<div class="col-auto d-none d-lg-block">
+ 						<iframe height="100%" src="https://www.youtube.com/embed/8eekJ-SarQo?si=Nv9-vxqsM7w6goYk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ 					</div>
+ 				</div>
+ 			</div>
+
+ 		</div>
+
+ 	</div>
+
+ </main> <!-- /container -->
